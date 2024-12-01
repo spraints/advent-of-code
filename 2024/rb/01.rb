1a580b7e07bd10f2e23080e6bb738f3f8e1c24c7
Matt Burke <spraints@gmail.com>
2024-12-01 14:39:30 -0500

    less code for part 2

--- 2024/rb/01.rb
+++ 2024/rb/01.rb
@@ -5,26 +5,6 @@ b = input.map { _2 }.sort
 puts "part 1: #{a.zip(b).map { (_1 - _2).abs }.sum}"
 
 # Check if there are any repeats in the first list.
-# p a: a.size, au: a.uniq.size
-
-# p b: b
-total = 0
-i = 0
-mem = {}
-a.each do |x|
-  if mem[x]
-    # p x => mem[x]
-    total += mem[x]
-  elsif i < b.size
-    while b[i] < x
-      i += 1
-    end
-    j = i
-    while b[i] == x
-      i += 1
-    end
-    total += mem[x] = x * (i - j)
-    # p a: x, i: i, j: j, c: (i - j)
-  end
-end
-puts "part 2: #{total}" # 21271939
+bc = b.group_by(&:itself).transform_values(&:size)
+bc.default = 0
+puts "part 2: #{a.sum { bc[_1] * _1 }}"
