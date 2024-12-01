16166c35bf3726bc71a121b11cbfcb16ee37bfcb
Matt Burke <spraints@gmail.com>
2024-12-01 16:54:47 -0500

    day 1 part 2 in python

--- 2024/py/01.py
+++ 2024/py/01.py
@@ -1,9 +1,13 @@
 import fileinput
+from collections import Counter
 
 input = [[int(x) for x in line.strip().split()] for line in fileinput.input()]
-#print(input)
 
 list_a = sorted([x[0] for x in input])
 list_b = sorted([x[1] for x in input])
 part1 = sum(abs(a-b) for a, b in zip(list_a, list_b))
 print("Part 1:", part1)
+
+counts = Counter(list_b)
+part2 = sum(x * counts[x] for x in list_a)
+print("Part 2:", part2)
