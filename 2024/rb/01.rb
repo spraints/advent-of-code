47c9fd1fd2812032d3fef2863f1aee037d591452
Matt Burke <spraints@gmail.com>
2024-12-01 14:37:41 -0500

    day 1 part 2

--- 2024/rb/01.rb
+++ 2024/rb/01.rb
@@ -1,4 +1,30 @@
 input = ARGF.readlines.map { |line| line.split(/\s+/).map(&:to_i) }
-a = input.map { _1.first }
-b = input.map { _2 }
-puts a.sort.zip(b.sort).map { (_1 - _2).abs }.sum
+a = input.map { _1.first }.sort
+b = input.map { _2 }.sort
+
+puts "part 1: #{a.zip(b).map { (_1 - _2).abs }.sum}"
+
+# Check if there are any repeats in the first list.
+# p a: a.size, au: a.uniq.size
+
+# p b: b
+total = 0
+i = 0
+mem = {}
+a.each do |x|
+  if mem[x]
+    # p x => mem[x]
+    total += mem[x]
+  elsif i < b.size
+    while b[i] < x
+      i += 1
+    end
+    j = i
+    while b[i] == x
+      i += 1
+    end
+    total += mem[x] = x * (i - j)
+    # p a: x, i: i, j: j, c: (i - j)
+  end
+end
+puts "part 2: #{total}" # 21271939
