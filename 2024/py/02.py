8a32e8b4b8d1880fd980624334b5b9785ce57966
Matt Burke <spraints@gmail.com>
2024-12-03 22:30:35 -0500

    reformulate day 2 part 1 in python

--- 2024/py/02.py
+++ 2024/py/02.py
@@ -3,22 +3,37 @@ import fileinput
 # each row is a report, each number is a level.
 input = [[int(x) for x in line.strip().split()] for line in fileinput.input()]
 
-def all_increasing(report):
-    for a, b in zip(report, report[1:]):
-        d = b - a
-        if d > 3 or d < 1:
-            return False
-    return True
+# ex 1: 1
+# ex 2: 4
+# real 1: 624
 
-def all_decreasing(report):
-    for a, b in zip(report, report[1:]):
-        d = a - b
-        if d > 3 or d < 1:
-            return False
-    return True
+def is_safe(report):
+    #print(report)
+    if report[0] < report[1]:
+        #print("ASC")
+        for a, b in zip(report, report[1:]):
+            d = b - a
+            if d < 1 or d > 3:
+                return False
+        #print("SAFE")
+        return True
+    elif report[0] > report[1]:
+        #print("DESC")
+        for a, b in zip(report, report[1:]):
+            d = a - b
+            if d < 1 or d > 3:
+                return False
+        #print("SAFE")
+        return True
+    else:
+        return False
 
-safe = 0
-for report in input:
-    if all_increasing(report) or all_decreasing(report):
-        safe += 1
-print("Part 1: {}".format(safe))
+safe = filter(is_safe, input)
+part1 = sum(1 for _ in safe)
+print("Part 1: {}".format(part1))
+
+#safe = 0
+#for report in input:
+#    if all_increasing(report, tolerance = 1) or all_decreasing(report, tolerance = 1):
+#        safe += 1
+#print("Part 2: {}".format(safe))
