4a1b6a8b4759d341d77f6ef54716d9757e7801ff
Matt Burke <spraints@gmail.com>
2024-12-03 22:30:29 -0500

    day 3 part 2

--- 2024/py/03.py
+++ 2024/py/03.py
@@ -1,4 +1,16 @@
 import fileinput, re
 input = "\n".join(fileinput.input())
+
 part1 = sum([int(a) * int(b) for a, b in re.findall(r'mul\((\d+),(\d+)\)', input)])
 print("Part 1: {}".format(part1))
+
+enabled = True
+part2 = 0
+for a, b, yes, no in re.findall(r"mul\((\d+),(\d+)\)|(do)\(\)|(don't)\(\)", input):
+    if yes != "":
+        enabled = True
+    elif no != "":
+        enabled = False
+    elif enabled:
+        part2 += int(a) * int(b)
+print("Part 2: {}".format(part2))
