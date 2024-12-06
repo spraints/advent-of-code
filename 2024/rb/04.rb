655407925c322964a11b8a8305df46dfb54bb1b4
Matt Burke <spraints@gmail.com>
2024-12-05 21:46:24 -0500

    day 4 part 2

--- 2024/rb/04.rb
+++ 2024/rb/04.rb
@@ -25,3 +25,21 @@ end
 
 part1 = $grid.each_index.sum { |x| $grid[x].each_index.sum { |y| countxmas(x, y) } }
 puts "Part 1: #{part1}"
+
+def x_mas?(x, y)
+  return false if x == 0 || y == 0
+  return false unless $grid[x][y] == "A"
+  tl = $grid[x-1][y+1]
+  tr = $grid[x+1][y+1]
+  br = $grid[x+1][y-1]
+  bl = $grid[x-1][y-1]
+  ms = [tl, tr, br, bl].count { |c| c == "M" }
+  ss = [tl, tr, br, bl].count { |c| c == "S" }
+  return false unless ms == 2 && ss == 2
+  tl == tr || tl == bl
+rescue NoMethodError # read: out of bounds
+  false
+end
+
+part2 = $grid.each_index.sum { |x| $grid[x].each_index.count { |y| x_mas?(x, y) } }
+puts "Part 2: #{part2}"
