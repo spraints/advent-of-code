4112040a16b37cad51ec4b3013b4ea27b73600ed
Matt Burke <spraints@gmail.com>
2024-12-05 23:04:12 -0500

    day 5 part 2

--- 2024/rb/05.rb
+++ 2024/rb/05.rb
@@ -1,6 +1,7 @@
 require "set"
 
-$verboase = false
+$verbose = false
+$verbose2 = false
 
 order, manuals = ARGF.read.split("\n\n")
 $rules = Hash.new { |h,k| h[k] = {followed_by: [], preceded_by: []} }
@@ -12,9 +13,8 @@ end
 manuals = manuals.lines.map { |line| line.strip.split(",").map(&:to_i) }
 
 def correct?(manual)
-  manual = manual.dup
   no = Set.new
-  while v = manual.shift
+  manual.each do |v|
     print "#{v} " if $verbose
     if no.include?(v)
       print "NO!" if $verbose
@@ -33,5 +33,63 @@ ensure
   puts if $verbose
 end
 
-part1 = manuals.sum { |m| correct?(m) ? m[m.size / 2] : 0 }
+def mid(a)
+  a[a.size / 2]
+end
+
+part1 = manuals.sum { |m| correct?(m) ? mid(m) : 0 }
 puts "Part 1: #{part1}"
+
+def corrected(manual)
+  return [0] if correct?(manual)
+
+  #chunks = []
+  #manual.each do |v|
+  #  if r = $rules[v]
+  #    before, after, dont_care = split_chunks(chunks, r)
+  #    if before.empty? && after.empty?
+  #      chunks << [v]
+  #    elsif before.empty?
+  #      chunks = [ [ v, after ] ] + dont_care
+  #    elsif after.empty?
+  #      chunks = [ [ before, v ] ] + dont_care
+  #    elsif before == after
+  #      raise "todo b==a: #{v} (#{r})\nchunks = #{chunks}\nbefore = #{before}\nafter = #{after}\ndont_care = #{dont_care}"
+  #    else
+  #      chunks = [ [ before, v, after ] ] + dont_care
+  #    end
+  #  else
+  #    chunks << [v]
+  #  end
+  #end
+  #res = chunks.flatten
+
+  res = manual.sort { |a, b|
+    if r = $rules[a]
+      if r[:preceded_by].include?(b)
+        # wrong order
+        1
+      elsif r[:followed_by].include?(b)
+        # right order
+        -1
+      else
+        0
+      end
+    else
+      0
+    end
+  }
+
+  puts "#{manual} should be #{res}" if $verbose2
+  res
+end
+
+def split_chunks(chunks, rule)
+  before = chunks.reject { |c| (c.flatten & rule[:preceded_by]).empty? }
+  after  = chunks.reject { |c| (c.flatten & rule[:followed_by]).empty? }
+  dont_care = chunks - before - after
+  [before, after, dont_care]
+end
+
+part2 = manuals.sum { |m| mid(corrected(m)) }
+puts "Part 2: #{part2}"
