4c19508544dca4e2d1306de9811b96c89e0e0bad
Matt Burke <spraints@gmail.com>
2024-12-06 13:40:25 -0500

    first idea for part 2, doesn't work
    i need to figure out other places. for example:
    
    ..#......
    ..^>>>>O.
    ..^......
    .#^......
    ......#..

--- 2024/py/06.py
+++ 2024/py/06.py
@@ -25,16 +25,24 @@ def turn_right(dir):
     return (dir[1], dir[0] * -1)
 
 visited = {}
+obstructions = set()
 while pos[0] >= 0 and pos[1] >= 0 and pos[0] < rows and pos[1] < cols:
     r, c = pos
     if grid[r][c] == "#":
-        # backtrack.
+        # backtrack. we were never here.
         pos = (r - dir[0], c - dir[1])
         # turn right 90 degrees.
         dir = turn_right(dir)
     else:
-        if pos not in visited:
-            visited[pos] = []
-        visited[pos].append(dir)
-        pos = (r + dir[0], c + dir[1])
+        next_pos = (r + dir[0], c + dir[1])
+        if pos in visited:
+            r = turn_right(dir)
+            if turn_right(dir) in visited[pos]:
+                obstructions.add(next_pos)
+        else:
+            visited[pos] = set()
+        visited[pos].add(dir)
+        pos = next_pos
 print("Part 1: {}".format(len(visited)))
+#print(obstructions)
+print("Part 2: {}".format(len(obstructions)))
