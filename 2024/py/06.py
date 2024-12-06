84233199279f3cc3f97af67033018e50bfc66168
Matt Burke <spraints@gmail.com>
2024-12-06 13:31:17 -0500

    track direction

--- 2024/py/06.py
+++ 2024/py/06.py
@@ -21,15 +21,18 @@ for r in range(rows):
 def inside(pos, rows, cols):
     return 
 
-part1 = set()
+def turn_right(dir):
+    return (dir[1], dir[0] * -1)
+
+visited = {}
 while pos[0] >= 0 and pos[1] >= 0 and pos[0] < rows and pos[1] < cols:
     r, c = pos
     if grid[r][c] == "#":
         # backtrack.
         pos = (r - dir[0], c - dir[1])
         # turn right 90 degrees.
-        dir = (dir[1], dir[0] * -1)
+        dir = turn_right(dir)
     else:
-        part1.add(pos)
+        visited[pos] = dir
         pos = (r + dir[0], c + dir[1])
-print("Part 1: {}".format(len(part1)))
+print("Part 1: {}".format(len(visited)))
