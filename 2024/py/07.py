4c9207dadb8c60df48facf9ef87340d36203e3d7
Matt Burke <spraints@gmail.com>
2024-12-07 14:09:06 -0500

    part 2
    there was another step where i was creating nested lists in part 2, and
    where i was taking the log of inputs[i] every time i multiplied it.

--- 2024/py/07.py
+++ 2024/py/07.py
@@ -14,5 +14,42 @@ def is_true(output, inputs):
         options = [opt for pair in options for opt in pair]
         i += 1
     return output in options
-part1 = sum([output for output, inputs in equations if is_true(output, inputs)])
+
+part1 = 0
+not_true = []
+for output, inputs in equations:
+    if is_true(output, inputs):
+        part1 += output
+    else:
+        not_true.append((output, inputs))
 print("Part 1: {}".format(part1))
+
+def cc1(r):
+    f = 10
+    x = r // 10
+    while x > 0:
+        f *= 10
+        x //= 10
+    return f
+
+def is_true2(output, inputs):
+    options = [inputs[0]]
+    i = 1
+    while i < len(inputs):
+        # All the new terms increase the value, so drop anything that's already over the limit.
+        options = [opt for opt in options if opt <= output]
+        new_opts = []
+        f = cc1(inputs[i])
+        for opt in options:
+            new_opts.append(opt + inputs[i])
+            new_opts.append(opt * inputs[i])
+            new_opts.append(f * opt + inputs[i])
+        options = new_opts
+        i += 1
+    return output in options
+
+part2 = part1
+for output, inputs in not_true:
+    if is_true2(output, inputs):
+        part2 += output
+print("Part 2: {}".format(part2))
