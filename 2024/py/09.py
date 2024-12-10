68a43d623dd78b6e5998bb07e3adab6726c43644
Matt Burke <spraints@gmail.com>
2024-12-09 22:49:38 -0500

    part 2, but 8439434080946 is too high

--- 2024/py/09.py
+++ 2024/py/09.py
@@ -6,14 +6,19 @@ def p(c):
         case _:
             return None
 digits = [d for d in (p(c) for line in fileinput.input() for c in line) if d is not None]
-positions = []
+orig_positions = []
+gaps = [] # dict of {size: set(positions)}
 sz = 0
 for i, x in enumerate(digits):
     if i % 2 == 0:
-        positions.append((i//2, sz, x))
+        orig_positions.append((i//2, sz, x))
         sz += x
     else:
+        while x >= len(gaps):
+            gaps.append(set())
+        gaps[x].add(sz)
         sz += x
+#print(gaps)
 
 def show(positions):
     s = ""
@@ -30,6 +35,7 @@ def csum(positions):
         csum += id * (start * w + w * (w - 1) // 2)
     return csum
 
+positions = orig_positions
 pos1 = []
 i = 0
 while len(positions) > 0:
@@ -50,6 +56,40 @@ while len(positions) > 0:
     # append this file.
     pos1.append((id, i, w))
     i += w
-#show(positions)
+#show(orig_positions)
 #show(pos1)
 print("Part 1: {}".format(csum(pos1)))
+
+def choose_gap(gaps, start, w):
+    best_idx = None
+    best_w = None
+    for off, idxs in enumerate(gaps[w:]):
+        if len(idxs) == 0:
+            continue
+        if best_idx is None:
+            best_idx = min(idxs)
+            best_w = w + off
+        else:
+            new_idx = min(idxs)
+            if new_idx < best_idx:
+                best_idx = new_idx
+                best_w = w + off
+    return best_idx, best_w
+
+pos2 = []
+for id, start, w in reversed(orig_positions):
+    new_start, gap_size = choose_gap(gaps, start, w)
+    if new_start is not None:
+        pos2.append((id, new_start, w))
+        gaps[gap_size].remove(new_start)
+        gaps[gap_size - w].add(new_start + w)
+    else:
+        pos2.append((id,start,w))
+
+def st(pos):
+    return pos[1]
+pos2 = sorted(pos2, key=st)
+#print(orig_positions)
+#print(pos2)
+#show(pos2)
+print("Part 2: {}".format(csum(pos2)))
