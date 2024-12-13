f106cf0735c8aa9ccd6e42143d767704253873a6
Matt Burke <spraints@gmail.com>
2024-12-12 22:39:45 -0500

    day 11: got it

--- 2024/py/11.py
+++ 2024/py/11.py
@@ -1,4 +1,4 @@
-import fileinput, os
+import fileinput
 
 nexts = {}
 def mem_blink(n):
@@ -7,15 +7,12 @@ def mem_blink(n):
     return nexts[n]
 
 def blink(n):
-    if type(n) is tuple:
-        a, b = n
-        return (mem_blink(a), mem_blink(b))
     if n == 0:
-        return 1
+        return [1]
     q, r = divmod(countdigits(n), 2)
     if r == 0:
         return divmod(n, 10**q)
-    return n * 2024
+    return [n * 2024]
 
 def countdigits(n):
     digits = 0
@@ -24,22 +21,23 @@ def countdigits(n):
         digits += 1
     return digits
 
-def count(x):
-    if type(x) is list or type(x) is tuple:
-        n = 0
-        for a in x:
-            n += count(a)
-        return n
-    else:
+expanded = {}
+def expand(n, blinks):
+    if blinks == 0:
         return 1
+    if (n, blinks) not in expanded:
+        total = 0
+        for b in mem_blink(n):
+            total += expand(b, blinks - 1)
+        expanded[(n, blinks)] = total
+    return expanded[(n, blinks)]
 
 numbers = [int(c) for c in fileinput.input().readline().split()]
-for _ in range(25):
-    for i in range(len(numbers)):
-        numbers[i] = mem_blink(numbers[i])
-print("Part 1: {}".format(count(numbers)))
-
-for _ in range(50):
-    for i in range(len(numbers)):
-        numbers[i] = mem_blink(numbers[i])
-print("Part 2: {}".format(count(numbers)))
+total = 0
+for n in numbers:
+    total += expand(n, 25)
+print("Part 1: {}".format(total))
+total = 0
+for n in numbers:
+    total += expand(n, 75)
+print("Part 2: {}".format(total))
