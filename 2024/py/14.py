f226b4a83b306453fb233374ff3d4822a3eb8fc1
Matt Burke <spraints@gmail.com>
2024-12-15 21:52:30 -0500

    oooooo kkkkkkkk

--- 2024/py/14.py
+++ 2024/py/14.py
@@ -2,38 +2,57 @@ import fileinput, re, os
 
 robots = [re.match(r"p=(\d+),(\d+) v=(-?\d+),(-?\d+)", line) for line in fileinput.input()]
 robots = [((int(x[1]),int(x[2])), (int(x[3]),int(x[4]))) for x in robots]
-size = (101, 103) if os.getenv("IS_REAL_INPUT") == "Y" else (11, 7)
+is_real_input = os.getenv("IS_REAL_INPUT") == "Y"
+size = (101, 103) if is_real_input else (11, 7)
 
-grid = [[0 for _ in range(size[0])] for _ in range(size[1])]
-final_positions = []
-for p, v in robots:
-    px, py = p
-    vx, vy = v
-    fpx = (px + 100 * vx) % size[0]
-    fpy = (py + 100 * vy) % size[1]
-    final_positions.append((fpx, fpy))
-    #print((fpx, fpy))
-    grid[fpy][fpx] += 1
-#for line in grid:
-#    for count in line:
-#        if count == 0:
-#            print(".", end="")
-#        else:
-#            print(count, end="")
-#    print("")
-qs = [0, 0, 0, 0]
-midx = size[0] // 2
-midy = size[1] // 2
-for fpx, fpy in final_positions:
-    if fpx < midx and fpy < midy:
-        qs[0] += 1
-    elif fpx > midx and fpy < midy:
-        qs[1] += 1
-    elif fpx < midx and fpy > midy:
-        qs[2] += 1
-    elif fpx > midx and fpy > midy:
-        qs[3] += 1
-res = qs[0]
-for q in qs[1:]:
-    res *= q
-print("Part 1: ", res)
+def show_grid(positions):
+    for y in range(size[1]):
+        for x in range(size[0]):
+            print(positions.get((x,y), "."), end="")
+        print("")
+
+def steps(n):
+    res = {}
+    for p, v in robots:
+        px, py = p
+        vx, vy = v
+        fp = ((px + n * vx) % size[0], (py + n * vy) % size[1])
+        if fp in res:
+            res[fp] += 1
+        else:
+            res[fp] = 1
+    return res
+
+def score(positions):
+    qs = [0, 0, 0, 0]
+    midx = size[0] // 2
+    midy = size[1] // 2
+    for fp, count in positions.items():
+        fpx, fpy = fp
+        if fpx < midx and fpy < midy:
+            qs[0] += count
+        elif fpx > midx and fpy < midy:
+            qs[1] += count
+        elif fpx < midx and fpy > midy:
+            qs[2] += count
+        elif fpx > midx and fpy > midy:
+            qs[3] += count
+    res = qs[0]
+    for q in qs[1:]:
+        res *= q
+    return res
+
+part1_positions = steps(100)
+print("Part 1: ", score(part1_positions))
+
+if is_real_input:
+    needed = 2
+    for i in range(1, 100000):
+        pos = steps(i)
+        if len(pos) == len(robots):
+            x = steps(i)
+            show_grid(x)
+            print("Part 2: ", i, " ", score(x))
+            needed -= 1
+            if needed < 1:
+                break
