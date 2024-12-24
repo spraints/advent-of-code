b52840cbfe11925dbe17c5c0ff847c0ab87c4c15
Matt Burke <spraints@gmail.com>
2024-12-23 22:21:22 -0500

    here's the highest cost, but i actually need lowest

--- 2024/py/16.py
+++ 2024/py/16.py
@@ -1,5 +1,9 @@
 import fileinput, heapq
+
+VIZ = True
+
 grid = [[c for c in line.strip()] for line in fileinput.input()]
+
 s = None
 e = None
 for r, row in enumerate(grid):
@@ -8,5 +12,52 @@ for r, row in enumerate(grid):
             s = (r, c)
         if cell == 'E':
             e = (r, c)
-print(s)
-print(e)
+
+#      (-1,0)
+# (0,-1)  @  (0,1)
+#       (1,0)
+d = (0,1)
+
+options = [ (s,d,0,frozenset([s])) ]
+
+def trydir(p, nd, ns, v):
+    np = (p[0] + nd[0], p[1] + nd[1])
+    x = grid[np[0]][np[1]]
+    if x != "." and x != "E":
+        return
+    if np in v:
+        return
+    if VIZ:
+        print(" also check {} (dir={})".format(np, nd))
+    options.append( (np, nd, ns, v | frozenset([np])) )
+
+def show(score, p, v):
+    print("score={} vs {}, p={} end={}".format(score, best, p, e))
+    for r, row in enumerate(grid):
+        vr = []
+        for c, x in enumerate(row):
+            if p == (r,c) or (r,c) in v:
+                vr.append("x")
+            else:
+                vr.append(x)
+        print("".join(vr))
+
+best = 0
+while len(options) > 0:
+    s, d, score, visited = options.pop()
+    if VIZ:
+        show(score, s, visited)
+    if s == e:
+        if VIZ:
+            print("END!!!!!")
+        if score > best:
+            best = score
+        continue
+
+    l = (-d[1], d[0])
+    r = (d[1], -d[0])
+    trydir(s, d, score + 1, visited)
+    trydir(s, l, score + 1001, visited)
+    trydir(s, r, score + 1001, visited)
+
+print("Part 1: ", best)
